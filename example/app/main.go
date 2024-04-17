32e94538b7d1d65e9944f6a585c6fb23d18aea94
prime <prime@localhost.localdomain>
2024-04-17 21:06:08 +0200

    [Elements]: elements are ready for reactive state

--- example/app/main.go
+++ example/app/main.go
@@ -2,24 +2,34 @@ package main
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/4lxprime/gtml"
 	. "github.com/4lxprime/gtml/elements"
 	"github.com/4lxprime/gtml/runtime"
 )
 
-func HeaderCmp(title string) Element {
-	return Div()()
-}
-
 func Index(app *gtml.App) *gtml.App {
 	paddingState := app.UseState(20)
+
+	clickP := P(
+		Style("color: blue;"),
+	)(
+		Text("Click on the button:"),
+	)
+
+	go func() {
+		time.Sleep(3 * time.Second)
+		fmt.Println("should change")
+		clickP.AppendChild(Text("tada"))
+		Update(clickP)
+	}()
 
 	return app.Use(
 		Div(
 			Style(
 				"background-color: cyan;",
-				"padding: 10px;",
+				fmt.Sprintf("padding: %vpx;", paddingState.Get()),
 			),
 		)(
 			gtml.If(1 == 3)(
@@ -28,26 +38,17 @@ func Index(app *gtml.App) *gtml.App {
 				Text("super cool"),
 			).Value(),
 
-			gtml.For(0, 4)(
-				func(i int) Element {
-					return HeaderCmp(fmt.Sprintf("AGIT Wasm %d", i))
-				},
-			),
-
 			gtml.Each2[string]([]string{
 				"test",
-				"Super test",
+				"super test",
+				"super mega test",
 			})(func(i int, a string) Element {
 				return P()(
 					Text(a),
 				)
 			}),
 
-			P(
-				Style("color: blue;"),
-			)(
-				Text("Click on the button:"),
-			),
+			clickP,
 			// here is an example of a custom element with custom attribute
 			// we may need a way to block unusable attributes with syntax
 			CustomElem[struct {
